bf865f8d580657386416cc611efd1c026a02217b
Nabin Prasad Dhungana <nabinprasaddhungana@gmail.com>
2025-06-10 09:47:32 +0545

    admin.py filled

--- sajilo_hajiri/faceapp/admin.py
+++ sajilo_hajiri/faceapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import (
+    User, Class, Subject, ClassSubject, StudentClassEnrollment,
+    AttendanceSession, AttendanceRecord, AttendanceAlert, AttendanceReport,
+    FaceEncoding
+)
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Class)
+admin.site.register(Subject)
+admin.site.register(ClassSubject)
+admin.site.register(StudentClassEnrollment)
+admin.site.register(AttendanceSession)
+admin.site.register(AttendanceRecord)
+admin.site.register(AttendanceAlert)
+admin.site.register(AttendanceReport)
+admin.site.register(FaceEncoding)
