7c3a896a2c603d074570ffe574b62797b307c324
Nabin Prasad Dhungana <nabinprasaddhungana@gmail.com>
2025-06-11 15:52:37 +0545

    name field added to User table(custom name field rather than the first and last names of abstractuser class

--- sajilo_hajiri/faceapp/models.py
+++ sajilo_hajiri/faceapp/models.py
@@ -10,9 +10,10 @@ class User(AbstractUser):
     ]
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
     avatar_url = models.URLField(blank=True, null=True)
+    name = models.CharField(max_length=150, blank=True, null=True)
 
     def __str__(self):
-        return self.username
+        return self.name or self.username
 
 
 class Class(models.Model):
